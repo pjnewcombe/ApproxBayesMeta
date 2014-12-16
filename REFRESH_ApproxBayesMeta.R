9a00ad905e9a52850306beec09b10a2c3a2704b2
pjnewcombe <pjnewcombe@gmail.com>
2014-12-16 18:38:37 +0000

    Adding extra files

---
+++ REFRESH_ApproxBayesMeta.R
@@ -0,0 +1,15 @@
+package.name <- 'ApproxBayesMeta'
+package.location <- '/Users/pauln/Dropbox/Work Projects/R Packages'
+github.username <-'pjnewcombe'
+commit.message <- NULL
+commit.message <- "Edited simulation functions for 100 block design."
+
+### --- Load Pmisc and refresh the package
+library(Pmisc)
+RefreshPackage(package.name=package.name,commit.message=commit.message)
+
+do.not.run <- FALSE
+if (do.not.run) {
+  library(devtools)
+  install_github(username="pjnewcombe", repo="ApproxBayesMeta")
+}
